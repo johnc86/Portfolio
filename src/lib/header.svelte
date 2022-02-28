5faea8b6c1110a00f619dc60bf51b4164d71f6d4
johnc86 <johnc86@gmail.com>
2022-02-28 13:27:07 +0000

    header bg now using clip path

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -7,8 +7,7 @@
 
 <header on:click={() => showMobMenu = !showMobMenu}>
     <div class="header__bg"></div>
-    <div class="nav__bg"></div>
-    
+
     <button class="nav__button" >
         <svg version="1.1" class="nav__icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  viewBox="0 0 122.879 103.609" enable-background="new 0 0 122.879 103.609" xml:space="preserve"><g><path fill-rule="evenodd" clip-rule="evenodd" d="M10.368,0h102.144c5.703,0,10.367,4.665,10.367,10.367v0 c0,5.702-4.664,10.368-10.367,10.368H10.368C4.666,20.735,0,16.07,0,10.368v0C0,4.665,4.666,0,10.368,0L10.368,0z M10.368,82.875 h102.144c5.703,0,10.367,4.665,10.367,10.367l0,0c0,5.702-4.664,10.367-10.367,10.367H10.368C4.666,103.609,0,98.944,0,93.242l0,0 C0,87.54,4.666,82.875,10.368,82.875L10.368,82.875z M10.368,41.438h102.144c5.703,0,10.367,4.665,10.367,10.367l0,0 c0,5.702-4.664,10.368-10.367,10.368H10.368C4.666,62.173,0,57.507,0,51.805l0,0C0,46.103,4.666,41.438,10.368,41.438 L10.368,41.438z"/></g></svg>
         <!-- <span class="nav__button-label">
@@ -21,6 +20,7 @@
             John Callaghan
         </div>
     </div>
+
 </header>
 <nav>
     <div class="nav__content">
@@ -47,16 +47,13 @@
     }
 
     .header__bg {
+        -webkit-clip-path: polygon(0 0,100% 0,100% 100%,0 40%);
+
+        width: 100%;
+        height: 100%;
         position: absolute;
-        width: 110%;
         background: $headerbg;
         z-index: 1;
-        left: -5%;
-        top: 50%;
-        height: 12rem;
-        transform:
-            rotate(8deg)
-            translateY(-80%);
     }
 
     .nav__button {
