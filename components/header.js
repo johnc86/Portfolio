db1aacb249a9ab13a8b3ba0a5c165a612051bab7
John Callaghan <john_callaghan@damibu.com>
2022-03-03 15:52:38 +0000

    state based nav working on mobile
    No longer showing on desktop

--- components/header.js
+++ components/header.js
@@ -1,19 +1,22 @@
 import styles from './header.module.scss'
+import Nav from './nav'
 import { useState } from 'react'
 
-// const [active, setActive] = useState(false);
-
-//     const handleClick = () => {
-//         setActive(!active);
-//     };
 
 export default function Header() {
-    return (<>
-        <header className={styles.header}>
+    const [mobileNav, navToggle] = useState(false);
+    const toggleNav = () => {
+        navToggle(!mobileNav);
+    }
+
+
+    return (
+    <>
+        <header className={styles.header} onClick={toggleNav}>
             <div className={styles.header__bg}></div>
 
             <button className={styles.nav__button} 
-                // onClick={handleClick}
+                onClick={toggleNav}
             >
                 =
             </button>
@@ -23,19 +26,10 @@ export default function Header() {
                     John Callaghan
                 </div>
             </div>
-
+        
         </header>
-        <nav>
-            <div className={styles.nav__content}>
-                <ul className={styles.nav__links}>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/posts">Blog</a></li>
-                    <li><a href="/work">Work</a></li>
-                    <li><a href="/career">Career</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
-            </div>
-        </nav>
+        
+        <Nav test={mobileNav}/>
     </>
     )
 };
